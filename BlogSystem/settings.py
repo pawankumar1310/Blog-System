b2e4b5ce466a97e67f3d95fe7610d97815a39639
pawan-nickelfox <pawankumar131098@gmail.com>
2021-12-25 19:10:39 +0530

    initialize sqlite

--- BlogSystem/settings.py
+++ BlogSystem/settings.py
@@ -91,19 +91,19 @@ WSGI_APPLICATION = 'BlogSystem.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-#     }
-# }
-
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'blogSystem',
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
+#         'NAME': 'blogSystem',
+#     }
+# }
 
 
 # Password validation
